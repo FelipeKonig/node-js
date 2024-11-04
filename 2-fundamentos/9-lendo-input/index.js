8eb5886404a49628a47afd731b5f73958f834d10
felipe konig <lipekonig@gmail.com>
2024-11-04 01:27:46 -0300

    lendo input de dados

---
+++ 2-fundamentos/9-lendo-input/index.js
@@ -0,0 +1,11 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+readline.question('Qual a sua linguagem preferida?', (language) => {
+  corretAnswer = language.toLowerCase() === 'java' ?
+    console.log(`A minha linguagem preferida é: ${language}`) :
+    console.log(`Tu é um jumento`)
+  readline.close()
+})
